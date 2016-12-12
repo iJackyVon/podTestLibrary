ceb6b3cdd81a76913f83ed7d58af6385d9d80f0c
Nemo <343191021@qq.com>
2016-12-12 22:01:41 +0800

    -s
    Initial Commit of Library

---
+++ podTestLibrary/Classes/UIColor+Cate.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Cate.h
+//  PandaFinancial
+//
+//  Created by Nemo on 15/12/31.
+//  Copyright © 2015年 nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Cate)
+
++(void)ironColor;
+
+@end
---
+++ podTestLibrary/Classes/UIColor+Cate.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+Cate.m
+//  PandaFinancial
+//
+//  Created by Nemo on 15/12/31.
+//  Copyright © 2015年 nemo. All rights reserved.
+//
+
+#import "UIColor+Cate.h"
+
+@implementation UIColor (Cate)
+
++(void)ironColor
+{
+    NSLog(@"ironColor");
+}
+
+@end
